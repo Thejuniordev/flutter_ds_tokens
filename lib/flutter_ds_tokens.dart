4ccca2a4f71d80f161583c41d82ffa184a1a8725
Junior Alves <junioralves.webdeveloper@gmail.com>
2021-09-21 16:19:08 -0300

    Bigfix

--- lib/flutter_ds_tokens.dart
+++ lib/flutter_ds_tokens.dart
@@ -2,21 +2,23 @@ library flutter_ds_tokens;
 
 /// A Tokens color
 class TokensColor {
-  TokensColor._();
+  TokensColor(tokensColor);
 
-    static const colorFeedbackError10 = #FADDC3;
-    static const colorFeedbackError50 = #ED8E36;
-    static const colorFeedbackSafety10 = #BADFF3;
-    static const colorFeedbackSafety50 = #1992D7;
-    static const colorFeedbackSuccess10 = #C9EBDD;
-    static const colorFeedbackSuccess50 = #4ABD8D;
-    static const colorKenloPrimary10 = #FDC0CB;
-    static const colorKenloPrimary100 = #CE2140;
-    static const colorKenloPrimary50 = #F82E52;
-    static const colorNeutral0 = #FFFFFF;
-    static const colorNeutral05 = #F2F2F2;
-    static const colorNeutral100 = #242424;
-    static const colorNeutral30 = #D8D8D8;
-    static const colorNeutral50 = #888888;
-    static const colorNeutral70 = #4C4C4C;
+  static TokensColor colorFeedbackError10 = TokensColor(0xFADDC3);
+
+  // static const colorFeedbackError10 = #FADDC3;
+  // static const colorFeedbackError50 = #ED8E36;
+  // static const colorFeedbackSafety10 = #BADFF3;
+  // static const colorFeedbackSafety50 = #1992D7;
+  // static const colorFeedbackSuccess10 = #C9EBDD;
+  // static const colorFeedbackSuccess50 = #4ABD8D;
+  // static const colorKenloPrimary10 = #FDC0CB;
+  // static const colorKenloPrimary100 = #CE2140;
+  // static const colorKenloPrimary50 = #F82E52;
+  // static const colorNeutral0 = #FFFFFF;
+  // static const colorNeutral05 = #F2F2F2;
+  // static const colorNeutral100 = #242424;
+  // static const colorNeutral30 = #D8D8D8;
+  // static const colorNeutral50 = #888888;
+  // static const colorNeutral70 = #4C4C4C;
 }
