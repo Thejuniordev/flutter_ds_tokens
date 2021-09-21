b9cc506f1848d54b514584debc93b1510eaa8d9e
Junior Alves <junioralves.webdeveloper@gmail.com>
2021-09-21 18:17:40 -0300

    Insert new model

--- lib/flutter_ds_tokens.dart
+++ lib/flutter_ds_tokens.dart
@@ -3,7 +3,19 @@ library flutter_ds_tokens;
 import 'package:flutter/material.dart';
 
 /// A Tokens color
-const Color kcPrimaryColor = Color(0xff22A45D);
-const Color kcMediumGreyColor = Color(0xff868686);
-const Color kcLightGreyColor = Color(0xffe5e5e5);
-const Color kcVeryLightGreyColor = Color(0xfff2f2f2);
+
+const Color colorFeedbackError10 = Color(0x33faddc3);
+const Color colorFeedbackError50 = Color(0xED8E36);
+const Color colorFeedbackSafety10 = Color(0xBADFF3);
+const Color colorFeedbackSafety50 = Color(0x1992D7);
+const Color colorFeedbackSuccess10 = Color(0xC9EBDD);
+const Color colorFeedbackSuccess50 = Color(0x4ABD8D);
+const Color colorKenloPrimary10 = Color(0xFDC0CB);
+const Color colorKenloPrimary100 = Color(0xCE2140);
+const Color colorKenloPrimary50 = Color(0xF82E52);
+const Color colorNeutral0 = Color(0xFFFFFF);
+const Color colorNeutral05 = Color(0xF2F2F2);
+const Color colorNeutral100 = Color(0x242424);
+const Color colorNeutral30 = Color(0xD8D8D8);
+const Color colorNeutral50 = Color(0x888888);
+const Color colorNeutral70 = Color(0x4C4C4C);
