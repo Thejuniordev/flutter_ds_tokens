9309072ead8964f7f9e9a5dd95622cf17c0f3165
Junior Alves <junioralves.webdeveloper@gmail.com>
2021-09-21 18:08:29 -0300

    remove class

--- lib/flutter_ds_tokens.dart
+++ lib/flutter_ds_tokens.dart
@@ -3,22 +3,7 @@ library flutter_ds_tokens;
 import 'package:flutter/material.dart';
 
 /// A Tokens color
-class TokensColor {
-  TokensColor(tokensColor);
-
-  static TokensColor color1 = TokensColor(0xfaddc3);
-  static Color colorFeedbackError50 = const Color(0xED8E36);
-  static Color colorFeedbackSafety10 = const Color(0xBADFF3);
-  static Color colorFeedbackSafety50 = const Color(0x1992D7);
-  static Color colorFeedbackSuccess10 = const Color(0xC9EBDD);
-  static Color colorFeedbackSuccess50 = const Color(0x4ABD8D);
-  static Color colorKenloPrimary10 = const Color(0xFDC0CB);
-  static Color colorKenloPrimary100 = const Color(0xCE2140);
-  static Color colorKenloPrimary50 = const Color(0xF82E52);
-  static Color colorNeutral0 = const Color(0xFFFFFF);
-  static Color colorNeutral05 = const Color(0xF2F2F2);
-  static Color colorNeutral100 = const Color(0x242424);
-  static Color colorNeutral30 = const Color(0xD8D8D8);
-  static Color colorNeutral50 = const Color(0x888888);
-  static Color colorNeutral70 = const Color(0x4C4C4C);
-}
+const Color kcPrimaryColor = Color(0xff22A45D);
+const Color kcMediumGreyColor = Color(0xff868686);
+const Color kcLightGreyColor = Color(0xffe5e5e5);
+const Color kcVeryLightGreyColor = Color(0xfff2f2f2);
