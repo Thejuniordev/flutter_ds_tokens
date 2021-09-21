7c86520ade37858fe8df56e16ba65107e55709ae
Junior Alves <junioralves.webdeveloper@gmail.com>
2021-09-21 17:10:10 -0300

    Bugfix

--- lib/flutter_ds_tokens.dart
+++ lib/flutter_ds_tokens.dart
@@ -6,19 +6,19 @@ import 'package:flutter/material.dart';
 class TokensColor {
   TokensColor(tokensColor);
 
-  static Color colorFeedbackError10 = Color(0xFADDC3);
-  static Color colorFeedbackError50 = Color(0xED8E36);
-  static Color colorFeedbackSafety10 = Color(0xBADFF3);
-  static Color colorFeedbackSafety50 = Color(0x1992D7);
-  static Color colorFeedbackSuccess10 = Color(0xC9EBDD);
-  static Color colorFeedbackSuccess50 = Color(0x4ABD8D);
-  static Color colorKenloPrimary10 = Color(0xFDC0CB);
-  static Color colorKenloPrimary100 = Color(0xCE2140);
-  static Color colorKenloPrimary50 = Color(0xF82E52);
-  static Color colorNeutral0 = Color(0xFFFFFF);
-  static Color colorNeutral05 = Color(0xF2F2F2);
-  static Color colorNeutral100 = Color(0x242424);
-  static Color colorNeutral30 = Color(0xD8D8D8);
-  static Color colorNeutral50 = Color(0x888888);
-  static Color colorNeutral70 = Color(0x4C4C4C);
+  static Color colorFeedbackError10 = const Color(0xfaddc3);
+  static Color colorFeedbackError50 = const Color(0xED8E36);
+  static Color colorFeedbackSafety10 = const Color(0xBADFF3);
+  static Color colorFeedbackSafety50 = const Color(0x1992D7);
+  static Color colorFeedbackSuccess10 = const Color(0xC9EBDD);
+  static Color colorFeedbackSuccess50 = const Color(0x4ABD8D);
+  static Color colorKenloPrimary10 = const Color(0xFDC0CB);
+  static Color colorKenloPrimary100 = const Color(0xCE2140);
+  static Color colorKenloPrimary50 = const Color(0xF82E52);
+  static Color colorNeutral0 = const Color(0xFFFFFF);
+  static Color colorNeutral05 = const Color(0xF2F2F2);
+  static Color colorNeutral100 = const Color(0x242424);
+  static Color colorNeutral30 = const Color(0xD8D8D8);
+  static Color colorNeutral50 = const Color(0x888888);
+  static Color colorNeutral70 = const Color(0x4C4C4C);
 }
