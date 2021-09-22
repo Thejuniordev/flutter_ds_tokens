cc288dec1fc2190d1529ee11cd8af514267d835f
Junior Alves <junioralves.webdeveloper@gmail.com>
2021-09-22 15:57:32 -0300

    Bugfix

--- lib/flutter_ds_tokens_font.dart
+++ lib/flutter_ds_tokens_font.dart
@@ -1,26 +1,23 @@
 library flutter_ds_tokens;
 
-class TokensAll {
-  TokensAll._();
+const fontFamilyKenlo = 'Manrope'; /* the Kenlo font-family */
+const fontSize12 = 12; /* the caption size of the font */
+const fontSize16 = 16; /* the body size of the font */
+const fontSize20 = 20; /* the subtitle size of the font */
+const fontSize24 = 24; /* the title size of the font */
+const fontSize32 = 32; /* the display size of the font */
+const fontWeightBold = 700; /* the bold weight of the font */
+const fontWeightMedium = 500; /* the medium weight of the font */
+const fontWeightSemibold = 600; /* the semibold weight of the font */
+const letterSpacing0 = 0; /* the letter-spacing of the font */
+const letterSpacingNeg1 = -0.4; /* the letter-spacing of the font */
+const letterSpacingNeg2 = -0.8; /* the letter-spacing of the font */
+const letterSpacingNeg3 = -1.2; /* the letter-spacing of the font */
+const lineHeight16 = 16; /* the line-height of the font */
+const lineHeight24 = 24; /* the line-height of the font */
+const lineHeight32 = 32; /* the line-height of the font */
+const lineHeight40 = 40; /* the line-height of the font */
+const textAlignCenter = 'center'; /* the text-align */
+const textAlignEnd = 'right'; /* the text-align */
+const textAlignStart = 'left'; /* the text-align */
 
-  static const fontFamilyKenlo = 'Manrope'; /* the Kenlo font-family */
-  static const fontSize12 = 12; /* the caption size of the font */
-  static const fontSize16 = 16; /* the body size of the font */
-  static const fontSize20 = 20; /* the subtitle size of the font */
-  static const fontSize24 = 24; /* the title size of the font */
-  static const fontSize32 = 32; /* the display size of the font */
-  static const fontWeightBold = 700; /* the bold weight of the font */
-  static const fontWeightMedium = 500; /* the medium weight of the font */
-  static const fontWeightSemibold = 600; /* the semibold weight of the font */
-  static const letterSpacing0 = 0; /* the letter-spacing of the font */
-  static const letterSpacingNeg1 = -0.4; /* the letter-spacing of the font */
-  static const letterSpacingNeg2 = -0.8; /* the letter-spacing of the font */
-  static const letterSpacingNeg3 = -1.2; /* the letter-spacing of the font */
-  static const lineHeight16 = 16; /* the line-height of the font */
-  static const lineHeight24 = 24; /* the line-height of the font */
-  static const lineHeight32 = 32; /* the line-height of the font */
-  static const lineHeight40 = 40; /* the line-height of the font */
-  static const textAlignCenter = 'center'; /* the text-align */
-  static const textAlignEnd = 'right'; /* the text-align */
-  static const textAlignStart = 'left'; /* the text-align */
-}
