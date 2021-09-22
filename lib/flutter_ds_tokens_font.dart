873bc8ff2eb5477712d3215c819ff1e50f14570e
Junior Alves <junioralves.webdeveloper@gmail.com>
2021-09-22 15:38:50 -0300

    Create new tokens font

---
+++ lib/flutter_ds_tokens_font.dart
@@ -0,0 +1,26 @@
+library flutter_ds_tokens;
+
+class TokensAll {
+  TokensAll._();
+
+  static const fontFamilyKenlo = 'Manrope'; /* the Kenlo font-family */
+  static const fontSize12 = 12; /* the caption size of the font */
+  static const fontSize16 = 16; /* the body size of the font */
+  static const fontSize20 = 20; /* the subtitle size of the font */
+  static const fontSize24 = 24; /* the title size of the font */
+  static const fontSize32 = 32; /* the display size of the font */
+  static const fontWeightBold = 700; /* the bold weight of the font */
+  static const fontWeightMedium = 500; /* the medium weight of the font */
+  static const fontWeightSemibold = 600; /* the semibold weight of the font */
+  static const letterSpacing0 = 0; /* the letter-spacing of the font */
+  static const letterSpacingNeg1 = -0.4; /* the letter-spacing of the font */
+  static const letterSpacingNeg2 = -0.8; /* the letter-spacing of the font */
+  static const letterSpacingNeg3 = -1.2; /* the letter-spacing of the font */
+  static const lineHeight16 = 16; /* the line-height of the font */
+  static const lineHeight24 = 24; /* the line-height of the font */
+  static const lineHeight32 = 32; /* the line-height of the font */
+  static const lineHeight40 = 40; /* the line-height of the font */
+  static const textAlignCenter = 'center'; /* the text-align */
+  static const textAlignEnd = 'right'; /* the text-align */
+  static const textAlignStart = 'left'; /* the text-align */
+}
