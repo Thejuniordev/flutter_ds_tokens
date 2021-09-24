b6264418d645109eb5e8a307ebaf1ea25133b6f8
Junior Alves <junioralves.webdeveloper@gmail.com>
2021-09-24 15:39:43 -0300

    Bugfix font weight

--- lib/flutter_ds_tokens_font.dart
+++ lib/flutter_ds_tokens_font.dart
@@ -3,22 +3,23 @@ library flutter_ds_tokens;
 import 'package:flutter/material.dart';
 
 const fontFamilyKenlo = 'Manrope'; /* the Kenlo font-family */
-const fontSize12 = 12; /* the caption size of the font */
-const fontSize16 = 16; /* the body size of the font */
-const fontSize20 = 20; /* the subtitle size of the font */
-const fontSize24 = 24; /* the title size of the font */
+const fontSize12 = 12.0; /* the caption size of the font */
+const fontSize16 = 16.0; /* the body size of the font */
+const fontSize20 = 20.0; /* the subtitle size of the font */
+const fontSize24 = 24.0; /* the title size of the font */
 const fontSize32 = 32.0; /* the display size of the font */
-const fontWeightBold = 700; /* the bold weight of the font */
-const fontWeightMedium = 500; /* the medium weight of the font */
-const fontWeightSemibold = 600; /* the semibold weight of the font */
+const fontWeightBold = FontWeight.w700; /* the bold weight of the font */
+const fontWeightMedium = FontWeight.w500; /* the medium weight of the font */
+const fontWeightSemibold =
+    FontWeight.w600; /* the semibold weight of the font */
 const letterSpacing0 = 0; /* the letter-spacing of the font */
 const letterSpacingNeg1 = -0.4; /* the letter-spacing of the font */
 const letterSpacingNeg2 = -0.8; /* the letter-spacing of the font */
 const letterSpacingNeg3 = -1.2; /* the letter-spacing of the font */
 const lineHeight16 = 1.16; /* the line-height of the font */
-const lineHeight24 = 24; /* the line-height of the font */
-const lineHeight32 = 32; /* the line-height of the font */
-const lineHeight40 = 40; /* the line-height of the font */
+const lineHeight24 = 1.24; /* the line-height of the font */
+const lineHeight32 = 1.32; /* the line-height of the font */
+const lineHeight40 = 1.40; /* the line-height of the font */
 const textAlignCenter = TextAlign.center; /* the text-align */
 const textAlignEnd = TextAlign.right; /* the text-align */
 const textAlignStart = TextAlign.left; /* the text-align */
